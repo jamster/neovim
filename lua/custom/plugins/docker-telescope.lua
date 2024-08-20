ad13c15aeafbaa779d87609ae34ecbf7f09b6b30
Jason Amster <jayamster@gmail.com>
2024-08-20 13:11:07 -0400

    Messing around with a docker telescope plugin
    https://youtu.be/HXABdG3xJW4?si=lBRJJoz9K7YIrA1C
    
    https://github.com/krisajenkins/telescope-docker.nvim

---
+++ lua/custom/plugins/docker-telescope.lua
@@ -0,0 +1,38 @@
+local pickers = require 'telescope.pickers'
+local finders = require 'telescope.finders'
+local previewers = require 'telescope.previewers'
+local config = require('telescope.config').values
+
+local M = {}
+
+M.show_docker_images = function(opts)
+  pickers
+    .new(opts, {
+      finder = finders.new_table {
+        results = {
+          { name = 'yes', value = { 1, 2, 3, 45 } },
+          { name = 'no', value = { 1, 2, 3, 45 } },
+          { name = 'maybe', value = { 1, 2, 3, 45 } },
+          { name = 'so', value = { 1, 2, 3, 45 } },
+        },
+        entry_maker = function(entry)
+          return {
+            display = entry.name,
+            ordinal = entry.name,
+          }
+        end,
+      },
+      sorter = config.generic_sorter(opts),
+      previewer = previewers.new_buffer_previewer {
+        title = 'Docker Image Details',
+        define_preview = function(self, entry)
+          vim.api.nvim_buf_set_lines(self.state.bufnr, 0, 0, true, { 'Hello', 'Everyone' })
+        end,
+      },
+    })
+    :find()
+end
+
+M.show_docker_images()
+
+return M
